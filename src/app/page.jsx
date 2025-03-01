354e8ca1666483a9a0eecf8e4e0a6f62584a6a2e
unknown <daniellsilva@outlook.com>
2025-03-01 13:20:34 -0300

    Finalizado listagem dos hóspedes

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,14 @@
 import { Flowbite } from "flowbite-react";
 import Header from "./view/component/header";
 import Dashboard from "./view/dashboard";
+import Guests from "./view/guests";
 
 export default function Home() {
 	return (
 		<Flowbite>
 			<main className="h-screen">
-				<Header />
-				<Dashboard />
+				<Header activePage={"guests"} />
+				<Guests />
 			</main>
 		</Flowbite>
 	)
