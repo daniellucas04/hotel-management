884f579c78474fcc128221ce8fdc97eab06170ca
unknown <daniellsilva@outlook.com>
2025-03-04 10:55:42 -0300

    Finalizado páginas de esboço e adicionado roteamento

---
+++ src/app/guests/page.jsx
@@ -0,0 +1,119 @@
+"use client";
+
+import { Badge, Table } from "flowbite-react";
+import Header from "../component/header";
+import Link from "next/link";
+
+export default function Guests() {
+  return (
+    <>
+      <Header />
+      <section className="overflow-x-auto m-10">
+        <h1 className="text-2xl mb-4">Todos os hóspedes</h1>
+        <Table striped>
+          <Table.Head>
+            <Table.HeadCell>Nome</Table.HeadCell>
+            <Table.HeadCell>Situação</Table.HeadCell>
+            <Table.HeadCell>Tipo de hospedagem</Table.HeadCell>
+            <Table.HeadCell>Quarto</Table.HeadCell>
+            <Table.HeadCell>
+              <span className="sr-only">Editar</span>
+            </Table.HeadCell>
+          </Table.Head>
+          <Table.Body className="divide-y">
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+              <Table.Cell className="flex items-center gap-4 whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                <img
+                  src="https://placehold.co/40x40"
+                  className="rounded-md"
+                  alt=""
+                />
+                <span>Henrique</span>
+              </Table.Cell>
+              <Table.Cell>
+                <Badge color="success" className="w-fit">
+                  Hospedado
+                </Badge>
+              </Table.Cell>
+              <Table.Cell>
+                <Badge color="blue" className="w-fit">
+                  Premium
+                </Badge>
+              </Table.Cell>
+              <Table.Cell className="font-bold">290</Table.Cell>
+              <Table.Cell className="flex items-center gap-4">
+                <Link href="/guests/1" className="font-medium text-cyan-600 hover:underline dark:text-cyan-500">
+                  Details
+                </Link>
+                <Link
+                  href="#"
+                  className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                >
+                  Editar
+                </Link>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+              <Table.Cell className="flex items-center gap-4 whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                <img
+                  src="https://placehold.co/40x40"
+                  className="rounded-md"
+                  alt=""
+                />
+                <span>Julia</span>
+              </Table.Cell>
+              <Table.Cell>
+                <Badge color="warning" className="w-fit">
+                  Reserva
+                </Badge>
+              </Table.Cell>
+              <Table.Cell>
+                <Badge color="blue" className="w-fit">
+                  Basico
+                </Badge>
+              </Table.Cell>
+              <Table.Cell className="font-bold">102</Table.Cell>
+              <Table.Cell>
+                <a
+                  href="#"
+                  className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                >
+                  Editar
+                </a>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+              <Table.Cell className="flex items-center gap-4 whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                <img
+                  src="https://placehold.co/40x40"
+                  className="rounded-md"
+                  alt=""
+                />
+                <span>Fernando</span>
+              </Table.Cell>
+              <Table.Cell>
+                <Badge color="dark" className="w-fit">
+                  Nenhuma situação
+                </Badge>
+              </Table.Cell>
+              <Table.Cell>
+                <Badge color="dark" className="w-fit">
+                  Nenhum tipo
+                </Badge>
+              </Table.Cell>
+              <Table.Cell></Table.Cell>
+              <Table.Cell>
+                <a
+                  href="#"
+                  className="font-medium text-cyan-600 hover:underline dark:text-cyan-500"
+                >
+                  Editar
+                </a>
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
+      </section>
+    </>
+  );
+}
