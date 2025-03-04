81fa156c2f21cab4bb8677125ea20fc6ece82078
unknown <daniellsilva@outlook.com>
2025-03-04 10:12:43 -0300

    página de produtos: listagem dos quartos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,13 +3,14 @@ import Header from "./view/component/header";
 import Dashboard from "./view/dashboard";
 import Guests from "./view/guests";
 import Bedrooms from "./view/bedrooms";
+import Products from "./view/products";
 
 export default function Home() {
 	return (
 		<Flowbite>
 			<main className="h-screen">
-				<Header activePage={"bedrooms"} />
-				<Bedrooms />
+				<Header activePage={"products"} />
+				<Products />
 			</main>
 		</Flowbite>
 	)
---
+++ src/app/view/products.jsx
@@ -0,0 +1,53 @@
+import { Card } from "flowbite-react";
+
+export default function Products() {
+  return (
+    <section className="flex flex-col items-center justify-center mt-10">
+        <h1 className="text-2xl font-bold">Selecione um quarto</h1>
+      <div className="grid grid-cols-4 gap-8 mt-8">
+        <Card className="w-[14rem] h-[12rem] hover:scale-105 cursor-pointer transition-all">
+          <h1 className="text-2xl font-bold text-center">{202}</h1>
+        </Card>
+        <Card className="w-[14rem] h-[12rem] hover:scale-105 cursor-pointer transition-all">
+          <h1 className="text-2xl font-bold text-center">{202}</h1>
+        </Card>
+        <Card className="w-[14rem] h-[12rem] hover:scale-105 cursor-pointer transition-all">
+          <h1 className="text-2xl font-bold text-center">{202}</h1>
+        </Card>
+        <Card className="w-[14rem] h-[12rem] hover:scale-105 cursor-pointer transition-all">
+          <h1 className="text-2xl font-bold text-center">{202}</h1>
+        </Card>
+        <Card className="w-[14rem] h-[12rem] hover:scale-105 cursor-pointer transition-all">
+          <h1 className="text-2xl font-bold text-center">{202}</h1>
+        </Card>
+        <Card className="w-[14rem] h-[12rem] hover:scale-105 cursor-pointer transition-all">
+          <h1 className="text-2xl font-bold text-center">{202}</h1>
+        </Card>
+        <Card className="w-[14rem] h-[12rem] hover:scale-105 cursor-pointer transition-all">
+          <h1 className="text-2xl font-bold text-center">{202}</h1>
+        </Card>
+        <Card className="w-[14rem] h-[12rem] hover:scale-105 cursor-pointer transition-all">
+          <h1 className="text-2xl font-bold text-center">{202}</h1>
+        </Card>
+        <Card className="w-[14rem] h-[12rem] hover:scale-105 cursor-pointer transition-all">
+          <h1 className="text-2xl font-bold text-center">{202}</h1>
+        </Card>
+        <Card className="w-[14rem] h-[12rem] hover:scale-105 cursor-pointer transition-all">
+          <h1 className="text-2xl font-bold text-center">{202}</h1>
+        </Card>
+        <Card className="w-[14rem] h-[12rem] hover:scale-105 cursor-pointer transition-all">
+          <h1 className="text-2xl font-bold text-center">{202}</h1>
+        </Card>
+        <Card className="w-[14rem] h-[12rem] hover:scale-105 cursor-pointer transition-all">
+          <h1 className="text-2xl font-bold text-center">{202}</h1>
+        </Card>
+        <Card className="w-[14rem] h-[12rem] hover:scale-105 cursor-pointer transition-all">
+          <h1 className="text-2xl font-bold text-center">{202}</h1>
+        </Card>
+        <Card className="w-[14rem] h-[12rem] hover:scale-105 cursor-pointer transition-all">
+          <h1 className="text-2xl font-bold text-center">{202}</h1>
+        </Card>
+      </div>
+    </section>
+  );
+}
