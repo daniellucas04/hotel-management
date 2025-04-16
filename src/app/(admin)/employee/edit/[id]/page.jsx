66aee0bd6dddb8ba965e02f56a7692c3a2038164
unknown <daniellsilva@outlook.com>
2025-04-15 21:49:28 -0300

    Correções nos Requisitos e Melhoria no frontend

---
+++ src/app/(admin)/employee/edit/[id]/page.jsx
@@ -0,0 +1,230 @@
+"use client";
+
+import {
+  HiCloudUpload,
+  HiLocationMarker,
+  HiOutlineAtSymbol,
+  HiOutlineBadgeCheck,
+  HiOutlineCalendar,
+  HiOutlineCollection,
+  HiOutlineIdentification,
+  HiOutlineKey,
+  HiOutlineLogin,
+  HiOutlineMail,
+  HiOutlinePhone,
+  HiOutlineViewGrid,
+} from "react-icons/hi";
+import {
+  Button,
+  Card,
+  FileInput,
+  HR,
+  Label,
+  Select,
+  TextInput,
+} from "flowbite-react";
+import { useState } from "react";
+
+export default function CreateEmployee() {
+  const [employeeData, setEmployeeData] = useState({
+    id_workgroup: 1,
+    name: "",
+    last_name: "",
+    document: "",
+    birthday: "",
+    phone1: "",
+    phone2: "",
+    address: "",
+    photo: "",
+    login: "",
+    password: "",
+    password_confirm: "",
+    email: "",
+    created_at: "",
+  });
+
+  function handleImageSubmit(event) {
+    console.log(event.target.files[0].name);
+  }
+
+  function handleData(event) {
+    setEmployeeData((p) => ({ ...p, [event.target.name]: event.target.value }));
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(employeeData);
+  }
+
+  return (
+    <>
+      <section className="overflow-x-auto m-10">
+        <h1 className="text-2xl mb-4">Editar funcionário</h1>
+        <Card>
+          <form
+            onSubmit={handleSubmit}
+            method="post"
+            className="flex flex-col gap-4"
+            encType="multipart/form-data"
+          >
+            <h1 className="text-xl font-bold flex items-center gap-2">
+              <HiOutlineViewGrid /> Informações gerais
+            </h1>
+            <div className="flex gap-4">
+              <TextInput
+                className="flex-auto"
+                icon={HiOutlineBadgeCheck}
+                placeholder="Nome"
+                name="name"
+                onChange={handleData}
+                value={employeeData.name}
+              />
+              <TextInput
+                className="flex-auto"
+                icon={HiOutlineBadgeCheck}
+                placeholder="Sobrenome"
+                name="last_name"
+                onChange={handleData}
+                value={employeeData.last_name}
+              />
+              <TextInput
+                className="flex-auto"
+                icon={HiOutlineIdentification}
+                placeholder="Documento"
+                name="document"
+                onChange={handleData}
+                value={employeeData.document}
+              />
+              <TextInput
+                className="flex-auto"
+                icon={HiOutlineCalendar}
+                placeholder="Data de nascimento"
+                name="birthday"
+                onChange={handleData}
+                value={employeeData.birthday}
+              />
+            </div>
+            <div className="flex gap-4">
+              <TextInput
+                className="flex-1"
+                icon={HiOutlinePhone}
+                placeholder="Telefone 1"
+                name="phone1"
+                onChange={handleData}
+                value={employeeData.phone1}
+              />
+              <TextInput
+                className="flex-1"
+                icon={HiOutlinePhone}
+                placeholder="Telefone 2"
+                name="phone2"
+                onChange={handleData}
+                value={employeeData.phone2}
+              />
+            </div>
+            <div className="flex gap-4">
+              <TextInput
+                className="flex-1"
+                icon={HiLocationMarker}
+                placeholder="Endereço"
+                name="address"
+                onChange={handleData}
+                value={employeeData.address}
+              />
+              <Select
+                className="flex-1"
+                icon={HiOutlineCollection}
+                placeholder="Cargo *"
+                name="id_workgroup"
+                onChange={handleData}
+                required
+              >
+                <option value="1">teste1</option>
+                <option value="2">teste2</option>
+                <option value="3">teste3</option>
+              </Select>
+            </div>
+            {/* Condição para mostrar a foto do funcionário caso exista */}
+            <div>
+              <Label className="text-lg" htmlFor="dropzone-file">
+                Foto do funcionário
+              </Label>
+              <Label
+                htmlFor="dropzone-file"
+                className="flex w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+              >
+                <div className="flex flex-col items-center justify-center pb-6 pt-5">
+                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                    <span className="flex items-center justify-center m-2">
+                      <HiCloudUpload size={35} />
+                    </span>
+                    <span className="font-semibold">Selecione uma imagem</span>{" "}
+                    ou arraste e solte
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">
+                    SVG, PNG ou JPG (MAX. 800x400px)
+                  </p>
+                </div>
+                <FileInput
+                  id="dropzone-file"
+                  name="photo"
+                  onChange={handleImageSubmit}
+                  className="hidden"
+                />
+              </Label>
+            </div>
+
+            <HR />
+            <h1 className="text-xl font-bold flex items-center gap-2">
+              <HiOutlineLogin /> Informações de login
+            </h1>
+            <div className="flex flex-col gap-4">
+              <div className="flex gap-4">
+                <TextInput
+                  className="flex-1"
+                  icon={HiOutlineAtSymbol}
+                  placeholder="Usuário para login"
+                  name="login"
+                  onChange={handleData}
+                  value={employeeData.login}
+                />
+                <TextInput
+                  className="flex-1"
+                  icon={HiOutlineMail}
+                  placeholder="E-mail"
+                  name="email"
+                  onChange={handleData}
+                  value={employeeData.email}
+                />
+              </div>
+              <div className="flex gap-4">
+                <TextInput
+                  className="flex-1"
+                  icon={HiOutlineKey}
+                  placeholder="Senha"
+                  name="password"
+                onChange={handleData}/>
+
+                <TextInput
+                  className="flex-1"
+                  icon={HiOutlineKey}
+                  placeholder="Confirme a senha"
+                  name="password_confirm"
+                onChange={handleData}/>
+
+              </div>
+            </div>
+
+            <HR />
+            <div className="flex items-center gap-4 justify-end">
+              <Button color="light">
+                <a href="/employee">Cancelar</a>
+              </Button>
+              <Button>Salvar</Button>
+            </div>
+          </form>
+        </Card>
+      </section>
+    </>
+  );
+}
---
+++ src/app/(admin)/tasks/details/[id]/page.jsx
@@ -0,0 +1,161 @@
+"use client";
+
+import { Button, HR } from "flowbite-react";
+import { useState } from "react";
+import {
+  HiOutlineArrowLeft,
+  HiOutlineClock,
+  HiOutlineMail,
+  HiOutlinePhone,
+  HiOutlineUser,
+  HiOutlineUsers,
+} from "react-icons/hi";
+
+export default function TaskDetails() {
+  const [taskData, setTaskData] = useState({
+    id_employee: 0,
+    id_reservation: 0,
+    priority: "Baixa",
+    description: "",
+    price: 0.0,
+  });
+
+  return (
+    <>
+      
+      <section className="h-full mx-52 my-14">
+        <a
+          className="flex items-center gap-2 text-gray-600 hover:text-gray-400 transition-all"
+          href="/tasks"
+        >
+          <HiOutlineArrowLeft size={"16px"} />
+          Tarefas
+        </a>
+        <div className="flex items-center justify-between gap-4 mt-4">
+          <div className="flex items-center gap-4">
+            <img
+              src={taskData.photo_url}
+              className="max-w-20 max-h-2max-w-20 rounded-full"
+            />
+            <span className="text-3xl font-medium">{taskData.name}</span>
+          </div>
+          <div>
+            <Button color="light" size="sm">
+              <a href="/task/edit/1">Editar perfil</a>
+            </Button>
+          </div>
+        </div>
+        <HR />
+        <div className="flex justify-evenly">
+          <section className="flex flex-col items-start gap-4 text-gray-500">
+            <div className="flex items-center gap-4">
+              <span>
+                <HiOutlineClock />
+              </span>
+              <span className="text-zinc-900 font-medium">Nome completo</span>
+              <span>{taskData.fullName}</span>
+            </div>
+            <div className="flex items-center gap-4">
+              <span>
+                <HiOutlinePhone />
+              </span>
+              <span className="text-zinc-900 font-medium">Telefone</span>
+              <span>{taskData.phone}</span>
+            </div>
+            <div className="flex items-center gap-4">
+              <span>
+                <HiOutlineMail />
+              </span>
+              <span className="text-zinc-900 font-medium">E-mail</span>
+              <span>{taskData.fullName}</span>
+            </div>
+            <div className="flex items-center gap-4">
+              <span>
+                <HiOutlineUser />
+              </span>
+              <span className="text-zinc-900 font-medium">Nome completo</span>
+              <span>{taskData.fullName}</span>
+            </div>
+          </section>
+          <section className="flex flex-col items-start gap-4 text-gray-500">
+            <div className="flex items-center gap-4">
+              <span>
+                <HiOutlineUsers />
+              </span>
+              <span className="text-zinc-900 font-medium">Acompanhantes</span>
+              <span>{taskData.escorts}</span>
+            </div>
+            <div className="flex items-center gap-4">
+              <span>
+                <HiOutlineClock />
+              </span>
+              <span className="text-zinc-900 font-medium">Cliente desde</span>
+              <span>{taskData.hosting.since}</span>
+            </div>
+          </section>
+        </div>
+
+        <HR />
+
+        <div className="flex flex-col items-center justify-center mt-14">
+          <h1 className="text-lg font-bold">Plano do hóspede</h1>
+          <div className="flex gap-8">
+            <div className="text-xl mt-8 border px-12 py-8 rounded-md font-semibold scale-90 hover:scale-105 hover:cursor-default transition-all">
+              <p className="text-center text-2xl">{taskData.hosting.plan}</p>
+              <div className="mt-4">
+                Neste plano está incluso:
+                {taskData.hosting.details.map((value) => (
+                  <span
+                    key={value.service}
+                    className="flex flex-col text-md text-gray-500"
+                  >
+                    - {value.service}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-center mt-8">
+                <Button color="blue">Alterar plano</Button>
+              </div>
+            </div>
+            <div className="text-xl mt-8 border px-12 py-8 rounded-md font-semibold hover-90 hover:scale-110 hover:cursor-default transition-all">
+              <p className="text-center text-2xl">Normal</p>
+              <div className="mt-4">
+                Neste plano está incluso:
+                {taskData.hosting.details.map((value) => (
+                  <span
+                    key={value.service}
+                    className="flex flex-col text-md text-gray-500"
+                  >
+                    - {value.service}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-center mt-8">
+                <span className="text-sm border py-2 px-4 rounded-md text-gray-500 shadow">
+                  Plano atual
+                </span>
+              </div>
+            </div>
+            <div className="text-xl mt-8 border px-12 py-8 rounded-md font-semibold scale-90 hover:scale-105 hover:cursor-default transition-all">
+              <p className="text-center text-2xl">Premium</p>
+              <div className="mt-4">
+                Neste plano está incluso:
+                {taskData.hosting.details.map((value) => (
+                  <span
+                    key={value.service}
+                    className="flex flex-col text-md text-gray-500"
+                  >
+                    - {value.service}
+                  </span>
+                ))}
+              </div>
+              <div className="flex justify-center mt-8">
+                <Button color="blue">Alterar plano</Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
