3f13744430d6cd8e11f1d5ae243496d6e4db5de4
Vinicius <vivivezono@gmail.com>
2025-05-06 21:09:15 -0300

    Busca da imagem do employee corrigido

--- backend-api/src/modules/employees/employees.routes.js
+++ backend-api/src/modules/employees/employees.routes.js
@@ -1,37 +1,16 @@
 // vai definir as rotas para o controller 
 import express from 'express';
-import multer from 'multer';
-import path from 'path';
-import fs from 'fs';
 import { EmployeeController } from './employees.controller.js'
 import { upload } from '../../middlewares/upload.js';
 
-const uploadDir = path.resolve(process.cwd(), '../frontend/src/public/uploads');
 
-// Garante que o diretório existe
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
-
-// Configuração do multer
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    const uniqueName = `${Date.now()}.${file.mimetype.split('/')[1]}`;
-    cb(null, uniqueName);
-  },
-});
-
-// const upload = multer({ storage });
 const router = express.Router();
 
-router.post('/photo', upload.single('photo'), EmployeeController.create);
+// router.post('/photo', upload.single('photo'), EmployeeController.create);
 router.get('/', EmployeeController.getAll);
 router.get('/:id', EmployeeController.getById);
 router.post('/', EmployeeController.create);
-router.post('/:id/upload', upload.single('image'), EmployeeController.upload);
+router.post('/:id/uploads', upload.single('image'), EmployeeController.upload);
 router.put('/:id', EmployeeController.update);
 router.delete('/:id', EmployeeController.remove);
 
