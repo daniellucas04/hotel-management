b632880ad474c166d1d61c7fc1876a0909bf9c89
MateusSantos007 <163445278+MateusSantos007@users.noreply.github.com>
2025-05-27 13:55:37 -0300

    Update prismamiddleware.js

--- backend-api/src/middlewares/prismamiddleware.js
+++ backend-api/src/middlewares/prismamiddleware.js
@@ -1,18 +1,42 @@
 import { PrismaClient } from "@prisma/client";
-import { DateTime } from "luxon";
+import { DateTime, Zone } from "luxon";
 
 const prisma = new PrismaClient();
 
 prisma.$use(async (params, next) => {
 
   if (params.action === "create" && params.args?.data) {
-    // Se o campo não foi passado, definimos automaticamente
+
     if (!params.args.data.created_at) {
       params.args.data.created_at = DateTime.fromJSDate(new Date())
         .minus({hours: 3})
         .toJSDate();
     }
 
+    if(!params.args.data.updated_at && params.model !== "reservations") {
+      params.args.data.updated_at = DateTime.fromJSDate(new Date())
+        .minus({hours: 3})
+        .toJSDate();
+    }
+
+  }
+
+  if(params.action === "update" && params.args?.data) {
+
+    params.args.data.updated_at = DateTime.fromJSDate(new Date())
+      .minus({hours: 3})
+      .toJSDate();
+
+  }
+
+  if(params.model === "reservations" && (params.action === "create" || params.action === "update")){
+    params.args.data.check_in = DateTime.fromJSDate(params.args.data.check_in)
+      .minus({hours: 3})
+      .toJSDate();
+    
+    params.args.data.check_out = DateTime.fromJSDate(params.args.data.check_out)
+      .minus({hours: 3})
+      .toJSDate();
   }
 
   return next(params);
